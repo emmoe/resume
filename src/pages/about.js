298ee9760a5e4c9c1217be7e8fb0cf6f804019d9
Emma Yaffe <emyaff-emmoe@gmail.com>
2021-02-20 16:31:21 -0600

    Add home and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Emma</h1>
+      <p>Software slinging sorcerer.</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Welcome to the future home of my resume!</div>
 }
